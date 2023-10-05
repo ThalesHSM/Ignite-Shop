afffd7533c90cdf422d2216abcd2e994d1a0bf4e
ThalesHSM <thsm271@gmail.com>
2023-10-04 23:26:11 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import { globalStyles } from '../styles/global'
+import Image from 'next/image'
 
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="Logo" width={150} height={80}/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
